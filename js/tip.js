f4acf4d22106e67e2b0876e2da2ef2ef3bbd216a
gabatk <gabriela.trybuszek@gmail.com>
2022-12-12 23:20:27 +0100

    Tip calculator completed.

--- js/tip.js
+++ js/tip.js
@@ -1,19 +1,27 @@
 const score = document.querySelector('.score__value');
 const insertedValue = document.querySelector('.tip__placeholder');
 const tipBtns = document.querySelectorAll('.tip__rate-btn');
+const otherValue = document.querySelector('.tip__rate-other');
+const otherBtn = document.querySelector('.tip__rate-other-btn');
 
 const suggestedValue = () => {
 	tipBtns.forEach(tipBtn => {
 		tipBtn.addEventListener('click', () => {
-			if (tipBtn.innerHTML.endsWith('%')) {
-				const rate = tipBtn.innerHTML.slice(0, tipBtn.innerHTML.length - 1);
-				const billValue = insertedValue.value;
-				score.innerHTML = eval((rate / 100) * billValue);
-				tipBtn.innerHTML = '';
-				console.log(rate);
-				console.log(billValue);
-			}
+			let rate = tipBtn.innerHTML.slice(0, tipBtn.innerHTML.length - 1);
+			let billValue = insertedValue.value;
+			score.innerHTML = 'Your tip: ' + eval((rate / 100) * billValue);
+			insertedValue.value = '';
 		});
 	});
 };
+
+const differentValue = () => {
+	let rate = otherValue.value;
+	let billValue = insertedValue.value;
+	score.innerHTML = 'Your tip: ' + eval((rate / 100) * billValue);
+	insertedValue.value = '';
+	otherValue.value = '';
+};
+
 suggestedValue();
+otherBtn.addEventListener('click', differentValue);
