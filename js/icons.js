f2429ce2975445edda2ef5d6ecfcc8c852a6dd10
gabatk <gabriela.trybuszek@gmail.com>
2022-12-13 22:24:33 +0100

    Randomly icons changing completed.

--- js/icons.js
+++ js/icons.js
@@ -2,9 +2,18 @@ const sectionIcons = document.querySelector('.icons');
 const icons = sectionIcons.querySelectorAll('i');
 
 const randomNumber = () => {
+	let random;
 	do {
-		let random = Math.floor(Math.random() * 10);
+		random = Math.floor(Math.random() * 10);
 	} while (random === 10);
 
 	return random;
 };
+
+const iconClass = () => {
+	icons.forEach(icon => {
+		icon.classList.add('fa-' + randomNumber());
+	});
+};
+
+iconClass();
--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import * as simpleCalculator from './simple.js';
 import * as tempConverter from './temperature.js';
 import * as tipCalculator from './tip.js';
 import * as general from './general.js';
-import * as general from './icons.js';
+import * as icons from './icons.js';
